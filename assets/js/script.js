fd4efc08fb1d2694c52852c75d62492f23bac296
Kelsey Frahm <k.rae.heider@gmail.com>
2024-06-10 11:43:30 -0500

    added local storage functionality, still working out bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const searchButton = document.getElementById('searchButton')
 const pastSearches = document.getElementById('pastSearches')
 const currentWeatherEl = document.getElementById('currentWeather')
 const fiveDay = document.getElementById ('fiveDay')
-const pastButton = document.getElementsByClassName('pastButton')
+const pastButton = document.querySelector('.pastButton')
 
 const apiKeyLat = '3f163666b5dde31e5636f1163162fb15bed1633'
 const cityUrl = 'https://api.geocod.io/v1.7/geocode?'
@@ -16,15 +16,30 @@ const searchArray = []
 const coordArray = []
 const currentArray = []
 const fiveDayArray = []
-let date = new Date().toLocaleDateString();
+function addDays(date, days) {
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() + days);
+    return newDate;
+}
+const todayDate = new Date();
 
 
-
+document.body.addEventListener('click', function (event) {
+    if (event.target.className == 'pastButton') {
+        searchArray.unshift(pastButton.id)
+        console.log(searchArray)
+  
+    getLat()
+    clearSearch()
+    currentWeather()
+    }
+})
 
 searchButton.addEventListener('click', function buttonClick(){
     searchArray.unshift(cityInputEl.value)
     console.log(searchArray)
     
+    storeArray()
     getLat()
     clearSearch()
     currentWeather()
@@ -78,12 +93,10 @@ function currentWeather(){
       .then(function (data) {
         currentWeatherEl.innerHTML = '';
         const code = currentArray[0].weather[0].icon
-        const toUnicode = code => String.fromCodePoint(parseInt(code, 16))
-        console.log(toUnicode(code))
     
         const div = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -91,7 +104,7 @@ function currentWeather(){
         div.className = 'currentCard'
         //h3.setAttribute ('weatherNow', current['name'])
         h3.textContent =  currentArray[0].name
-        icon.textContent = toUnicode(code)
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + currentArray[0].main.temp
         wind.textContent = 'Wind: ' + currentArray[0].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + currentArray[0].main.humidity + '%'
@@ -118,10 +131,12 @@ function currentWeather(){
       })
       .then(function (data) {
         fiveDay.innerHTML = '';
+        const code = fiveDayArray[0].list[5].weather[0].icon
+        const newDate = addDays(todayDate, 1)
     
         const day1 = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -129,9 +144,9 @@ function currentWeather(){
         day1.className = 'fiveDayCard'
         day1.id = 'day1'
         //h3.setAttribute ('weatherNow', current['name'])
-        h3.textContent =  date
+        h3.textContent =  newDate.toDateString()
         h3.className = 'fiveDayHeader'
-        icon.textContent = currentArray[0].weather[0].icon
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + fiveDayArray[0].list[5].main.temp
         wind.textContent = 'Wind: ' + fiveDayArray[0].list[5].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + fiveDayArray[0].list[5].main.humidity + '%'
@@ -145,10 +160,12 @@ function currentWeather(){
         
     })
     .then(function (data) {
-    
+        const code = fiveDayArray[0].list[13].weather[0].icon
+        const newDate = addDays(todayDate, 2)
+
         const day2 = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -156,9 +173,9 @@ function currentWeather(){
         day2.className = 'fiveDayCard'
         day2.id = 'day2'
         //h3.setAttribute ('weatherNow', current['name'])
-        h3.textContent =  date
+        h3.textContent =  newDate.toDateString()
         h3.className = 'fiveDayHeader'
-        icon.textContent = currentArray[0].weather[0].icon
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + fiveDayArray[0].list[13].main.temp
         wind.textContent = 'Wind: ' + fiveDayArray[0].list[13].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + fiveDayArray[0].list[13].main.humidity + '%'
@@ -172,10 +189,12 @@ function currentWeather(){
         
     })
     .then(function (data) {
+        const code = fiveDayArray[0].list[21].weather[0].icon
+        const newDate = addDays(todayDate, 3)
     
         const day3 = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -183,9 +202,9 @@ function currentWeather(){
         day3.className = 'fiveDayCard'
         day3.id = 'day3'
         //h3.setAttribute ('weatherNow', current['name'])
-        h3.textContent =  fiveDayArray[0].list[21].dx_txt
+        h3.textContent =  newDate.toDateString()
         h3.className = 'fiveDayHeader'
-        icon.textContent = currentArray[0].weather[0].icon
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + fiveDayArray[0].list[21].main.temp
         wind.textContent = 'Wind: ' + fiveDayArray[0].list[21].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + fiveDayArray[0].list[21].main.humidity + '%'
@@ -199,10 +218,12 @@ function currentWeather(){
         
     })
     .then(function (data) {
+        const code = fiveDayArray[0].list[29].weather[0].icon
+        const newDate = addDays(todayDate, 4)
     
         const day4 = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -210,9 +231,9 @@ function currentWeather(){
         day4.className = 'fiveDayCard'
         day4.id = 'day4'
         //h3.setAttribute ('weatherNow', current['name'])
-        h3.textContent =  fiveDayArray[0].list[29].dx_txt
+        h3.textContent =  newDate.toDateString()
         h3.className = 'fiveDayHeader'
-        icon.textContent = currentArray[0].weather[0].icon
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + fiveDayArray[0].list[29].main.temp
         wind.textContent = 'Wind: ' + fiveDayArray[0].list[29].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + fiveDayArray[0].list[29].main.humidity + '%'
@@ -227,10 +248,12 @@ function currentWeather(){
     })
 
     .then(function (data) {
+        const code = fiveDayArray[0].list[37].weather[0].icon
+        const newDate = addDays(todayDate, 5)
     
         const day5 = document.createElement('div');
         const h3 =  document.createElement('h3');
-        const icon = document.createElement('p')
+        const icon = document.createElement('img')
         const temp = document.createElement('p')
         const wind = document.createElement('p')
         const humid = document.createElement('p')
@@ -238,9 +261,9 @@ function currentWeather(){
         day5.className = 'fiveDayCard'
         day5.id = 'day5'
         //h3.setAttribute ('weatherNow', current['name'])
-        h3.textContent =  fiveDayArray[0].list[37].dx_txt
+        h3.textContent =  newDate.toDateString()
         h3.className = 'fiveDayHeader'
-        icon.textContent = currentArray[0].weather[0].icon
+        icon.src = './assets/images/icons/'+code+'.png'
         temp.textContent = 'Temp: ' + fiveDayArray[0].list[37].main.temp
         wind.textContent = 'Wind: ' + fiveDayArray[0].list[37].wind.speed + 'mph'
         humid.textContent = 'Humidity: ' + fiveDayArray[0].list[37].main.humidity + '%'
@@ -280,76 +303,22 @@ function pastSearchList() {
 
 }
 
-// function currentCard() {
-//     currentWeatherEl.innerHTML = '';
-   
-//     const current = {
-//         name: cityInputEl,
-//         // icon: currentArray[1][0].icon,
-//         temp: currentArray[3].temp
+function storeArray (){
+    localStorage.setItem('searchArray', JSON.stringify(searchArray));
+}
 
-//     }
-
-//     console.log(current)
-//     const div = document.createElement('div');
-//     const h3 =  document.createElement('h3');
-//     const icon = document.createElement('p')
-//     const temp = document.createElement('p')
-//     const wind = document.createElement('p')
-//     const humid = document.createElement('p')
-
-//     div.className = 'currentCard'
-//     //h3.setAttribute ('weatherNow', current['name'])
-//     h3.textContent =  current.name
-//     icon.textContent = 'weather icon'
-//     temp.textContent = 'Temp:'
-//     wind.textContent = 'Wind:'
-//     humid.textContent = 'Humidity:'
-
-
-
-//     currentWeatherEl.appendChild(div)
-//     div.appendChild(h3)
-//     div.appendChild(icon)
-//     div.appendChild(temp)
-//     div.appendChild(wind)
-//     div.appendChild(humid)
-// }
-
-// function forecastCard() {
-//     fiveDay.innerHTML = '';
-   
-//     for (let i = 1; i < 6; i++) {
-//         const days = fiveDayArray[i]
-
-//     const div = document.createElement('div');
-//     const h3 =  document.createElement('h3');
-//     const icon = document.createElement('p')
-//     const temp = document.createElement('p')
-//     const wind = document.createElement('p')
-//     const humid = document.createElement('p')
-//     div.className = 'currentCard'
-//     //h3.setAttribute ('weatherNow', current['name'])
-//     h3.textContent =  'Tomorrow'
-//     icon.textContent = days.list[0]
-//     temp.textContent = 'Temp:'
-//     wind.textContent = 'current wind'
-//     humid.textContent = 'current humidity'
-
-
-
-//     fiveDay.appendChild(div)
-//     div.appendChild(h3)
-//     div.appendChild(icon)
-//     div.appendChild(temp)
-//     div.appendChild(wind)
-//     div.appendChild(humid)
-//     }
-// }
-
-
+function getArray(){
+    searchArray.push(JSON.parse(localStorage.getItem('searchArray')))
+}
 
 function clearSearch(event){
     document.getElementById(cityInputEl.value = '');
 };
 
+
+window.addEventListener("load", (event) => {
+    getArray()
+    pastSearchList()
+    console.log("page is fully loaded");
+    console.log(searchArray)
+  });
